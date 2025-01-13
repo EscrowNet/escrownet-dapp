69297de8a5c91cc60d588a456489870213b73734
Raul Allende <47288809+raulallende2025@users.noreply.github.com>
2025-01-12 22:50:54 -0500

    Creating route /dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-60 bg-white">{/* Sidebar */}</div>
+      <div className="flex-grow md:overflow-y-auto bg-gray-light">
+        {children}
+      </div>
+    </div>
+  );
+}
