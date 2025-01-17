0ecc9b945ce36251ea235f99496cbbc91e395686
Kefas Kingsley <kefasiceking@gmail.com>
2025-01-17 10:23:33 +0100

    feat: add sidebar component for dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+      <div className="p-6">
+        <div className="mb-8">
+          <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+        </div>
+      </div>
+    );
+  }
