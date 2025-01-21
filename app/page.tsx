991a150117c2c98dca896d270556b90d0b565b93
Otaiki1 <otaikisadiq2804@gmail.com>
2025-01-21 23:18:52 +0100

    minor refac

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import Image from "next/image";
-import Header from "./Components/Header";
-import Hero from "./Components/Hero";
-import Footer from "./Components/Footer";
-
 export default function Home() {
-    return (
-        <div className="w-full h-screen bg-[#F5F5F5]">
-            <Header />
-            <Hero />
-            <Footer />
-        </div>
-    );
+    return <div>Page</div>;
 }
