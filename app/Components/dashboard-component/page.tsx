cf8028aca1d293a0b50f8ab077c90da96ff8f813
Otaiki1 <otaikisadiq2804@gmail.com>
2025-01-21 23:06:06 +0100

    refac: refactor codebase

--- app/Components/dashboard-component/page.tsx
+++ app/Components/dashboard-component/page.tsx
@@ -3,16 +3,16 @@ import Banner from "../banner";
 import Transactions from "../transactionList";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <div className="p-6">
-        <Banner />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-          <Transactions title="Recent Transactions" />
-          <Transactions title="Total Transactions" />
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Header />
+            <div className="p-6">
+                <Banner />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+                    <Transactions title="Recent Transactions" />
+                    <Transactions title="Total Transactions" />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
