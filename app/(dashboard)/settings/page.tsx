cbc6d9fc623270a293eae116669be2c5e8e86938
youngancient <tochyokoye@gmail.com>
2025-02-21 18:46:39 +0100

    mod: settings page layout done

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,11 +1,17 @@
+import DashboardContentContainer from "@/components/DashboardContentContainer";
 import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            <h3>Hi, This is the settings page</h3>
-        </div>
-    );
+  return (
+    <div className="p-6 w-full h-full">
+      <DashboardContentContainer
+        title="Escrow Transaction"
+        className="w-full min-h-[195px] mt-[1.5rem]"
+      >
+        <h3>Hello world</h3>
+      </DashboardContentContainer>
+    </div>
+  );
 };
 
 export default page;
