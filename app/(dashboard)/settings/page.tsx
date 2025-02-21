cc23bb460a4f6a048644ac09ef14ae5cc2df72df
youngancient <tochyokoye@gmail.com>
2025-02-21 19:01:20 +0100

    mod: styled page elements

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -5,10 +5,27 @@ const page = () => {
   return (
     <div className="p-6 w-full h-full">
       <DashboardContentContainer
-        title="Escrow Transaction"
-        className="w-full min-h-[195px] mt-[1.5rem]"
+        title="Settings"
+        className="w-full mt-[1rem] pb-[3rem]"
       >
-        <h3>Hello world</h3>
+        <div className="p-[1.5rem] pb-[3rem] flex flex-col gap-[2rem]">
+          <div className="flex justify-between items-center">
+            <h2 className="text-[#131313] font-sans font-[600] text-[1rem] leading-[1.5625rem] opacity-60">
+              Transaction History export
+            </h2>
+            <button
+              type="button"
+              className="text-[#26A17B] text-center font-sans text-base font-bold leading-[1.5625rem]"
+            >
+              Download
+            </button>
+          </div>
+          <div className="flex justify-between items-center">
+            <h2 className="text-[#131313] font-sans font-[600] text-[1rem] leading-[1.5625rem] opacity-60">
+              Escrow status update
+            </h2>
+          </div>
+        </div>
       </DashboardContentContainer>
     </div>
   );
